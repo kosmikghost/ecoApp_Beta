e9e8842cad298bef9ed2c48ba3b9d532cc6b7b88
Michel Cortes Ortiz <michelcortes199@gmail.com>
2019-11-09 02:31:17 -0300

    info reciclaje

--- app/src/main/java/com/madgoat/ecoapp_beta/ReciclajeCreativo.java
+++ app/src/main/java/com/madgoat/ecoapp_beta/ReciclajeCreativo.java
@@ -7,13 +7,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ReciclajeCreativo extends AppCompatActivity {
 
@@ -25,7 +25,7 @@ public class ReciclajeCreativo extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
